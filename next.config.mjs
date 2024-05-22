2e371c172bf27e7d953f8c52509be63e3851c660
AbdulHaseeb <abdullhaseebonline@gmail.com>
2024-05-22 19:50:00 +0500

    next config modified

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import nextTranslate from 'next-translate';
+import i18nConfig from './i18n';
+
+const nextConfig = {
+  ...nextTranslate(i18nConfig),
+};
 
 export default nextConfig;
+
